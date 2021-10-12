35ae0acca026a2bf7902c65098f05382988d5bd2
Angelica Moberg <angelica.moberg@hotmail.com>
2021-10-12 17:25:11 +0200

    Adding a list to the loop

--- cart.js
+++ cart.js
@@ -1,6 +1,6 @@
 
 // Lista för att testa
-/* let shoppingBag = [
+let shoppingBag = [
     {
         title: "iPhone X",
         Description: "Last years phone from Apple with a beautiful all display front.",
@@ -22,27 +22,43 @@
         Image: "SamsungS22.png",
         Price: 6000
     }
-] */
+]
 
 
 
+
+// Taking out main from html
 let main = document.getElementsByTagName("main")[0]
 
-
-// Create Title
+// Create Title "Kundvagn"
 let title = document.createElement("h1")
 title.classList.add("cartTitle")
 title.innerText = "Kundvagn"
 main.appendChild(title)
 
+// Container that wraps all the shopping items together
+let wrapper = document.createElement("div")
+    wrapper.classList.add("wrapper")
+    main.appendChild(wrapper)
 
 
+
+
+
+
+
+
+// Loop for the list with added items into the shopping cart    
+for(let i = 0 ; i < shoppingBag.length ; i++ ) {
+
+
+let shopping = shoppingBag[i]  
 
 
 // Container to added item
 let div = document.createElement("div")
     div.classList.add("containerAddedItem")
-    main.appendChild(div)
+    wrapper.appendChild(div)
 
 // Container to image 
 let imageContainer = document.createElement("div")
@@ -57,13 +73,13 @@ let productImg = document.createElement("img")
 
 // PhoneModel // Add the list to innerText!    
 let phoneModelText = document.createElement("h2")
-    phoneModelText.innerText = "iPhone X"
+    phoneModelText.innerText = shopping.title
     div.appendChild(phoneModelText)
 
 
 // Price on phone // Add the list to innerText!     
 let priceItem = document.createElement("h3")
-    priceItem.innerText = "4999 kr"
+    priceItem.innerText = shopping.Price + " kr"
     div.appendChild(priceItem)
 
 // Button
@@ -75,6 +91,14 @@ let buttonContainer = document.createElement("button")
 let buttonText = document.createElement("h4")
     buttonText.innerText = "Ta bort"
     buttonContainer.appendChild(buttonText)
+
+}
+
+
+
+
+
+
 
 // totalPrice // Add sum of the list in innerText "4999"!
 let totalPrice = document.createElement("h3")
@@ -95,9 +119,4 @@ let buttonCompletePurchaseText = document.createElement("h4")
 
 
 
-    
-function addedItemsInCart() {
-
-
-}
 
